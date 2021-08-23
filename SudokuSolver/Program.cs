4807fd4ec2f0dc67c63aa2c92fb645e2dbd89330
sequws <workspaceshare@gmail.com>
2021-08-23 23:20:43 +0200

    Add some sample sudoku boards

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -10,6 +10,50 @@ namespace SudokuSolver
     {
         static void Main(string[] args)
         {
+            int[,] boardEasy =
+            {
+                {2,0,5,0,0,9,0,0,4},
+                {0,0,0,0,0,0,3,0,7},
+                {7,0,0,8,5,6,0,1,0},
+                {4,5,0,7,0,0,0,0,0},
+                {0,0,9,0,0,0,1,0,0},
+                {0,0,0,0,0,2,0,8,5},
+                {0,2,0,4,1,8,0,0,6},
+                {6,0,8,0,0,0,0,0,0},
+                {1,9,0,2,0,0,7,0,8}
+            };
+
+            int[,] boarBad =
+            {
+                {2,3,0,0,0 },
+                {0,1,0,0,0 }
+            };
+
+            PrintBoard(boardEasy);
+            PrintBoard(boarBad);
+
+            Console.ReadLine();
+        }
+
+
+        static void PrintBoard(int[,] board)
+        {
+            if(board.GetLength(0) != board.GetLength(1))
+            {
+                Console.WriteLine("This is not a valid sudoku board!");
+                return;
+            }
+
+            int size = board.GetLength(0);
+            for(int r =0; r< size; r++)
+            {
+                for(int c=0; c< size; c++)
+                {
+                    Console.Write( $"{board[r,c]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
