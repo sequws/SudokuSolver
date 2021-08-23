ce88a2a923f34e4b086c9bd68fbc5e5467de8dc5
sequws <workspaceshare@gmail.com>
2021-08-23 23:59:39 +0200

    Add Solver class, and basic tests

---
+++ Solver.Test/Solver.Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SudokuSolver.Tests
+{
+    
+    public class SolverTest
+    {
+        [Fact]
+        public void IsSquareBoard_BadBoard_False()
+        {
+            //Arrange
+            int[,] badBoard =
+            {
+                {0,0,1,1 },
+                {0,2,3,4 }
+            };
+            //Act
+            Solver solver = new Solver();
+            var res = solver.IsSquareBoard(badBoard);
+
+            //Assert
+            Assert.False(res);
+        }
+
+        [Fact]
+        public void IsSquareBoard_Board4x4_True()
+        {
+            //Arrange
+            int[,] badBoard =
+            {
+                {0,0,1,1 },
+                {0,2,3,4 },
+                {0,2,3,4 },
+                {0,2,3,4 },
+            };
+            //Act
+            Solver solver = new Solver();
+            var res = solver.IsSquareBoard(badBoard);
+
+            //Assert
+            Assert.True(res);
+        }
+    }
+}
---
+++ SudokuSolver/Solver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    public class Solver
+    {
+        public List<int[,]> SolutionList => solutionList;
+        List<int[,]> solutionList = new List<int[,]>();
+
+        public Solver()  {}
+
+
+        public bool IsSquareBoard(int[,] board)
+        {
+            if (board.GetLength(0) != board.GetLength(1))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
